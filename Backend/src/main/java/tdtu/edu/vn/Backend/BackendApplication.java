3f3edc99822bdc6dffcee583f0cbfdb7c38a8229
Nguyễn Lê Gia Bảo <52000632@student.tdtu.edu.vn>
2023-12-01 21:47:51 +0700

    updated 1/12/2023

--- Backend/src/main/java/tdtu/edu/vn/Backend/BackendApplication.java
+++ Backend/src/main/java/tdtu/edu/vn/Backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import tdtu.edu.vn.Backend.Models.UsersModel;
 
@@ -17,8 +18,35 @@ public class BackendApplication implements CommandLineRunner {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 	
-	
+	@Autowired
+    UsersRepo userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    //Initialize admin account value
+    @Value("${user.default.avatar}")
+    private String defaultAvatar;
+    @Value("${admin.username}")
+    private String adminUsername;
+    @Value("${admin.password}")
+    private String adminPassword;
+    @Value("${admin.name}")
+    private String adminName;
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    //Create admin account
     @Override
     public void run(String... args) throws Exception {
+        if(userRepository.count() == 0){
+            UsersModel user = new UsersModel();
+            user.setUsername(adminUsername);
+            user.setName(adminName);
+            user.setEmail(adminEmail);
+            user.setAvatar(defaultAvatar);
+            user.setRoles("ROLE_USER,ROLE_ADMIN");
+            user.setPassword(passwordEncoder.encode(adminPassword));
+            userRepository.save(user);
+            System.out.println(user);
+        }
     }
 }
---
+++ Backend/src/main/java/tdtu/edu/vn/Backend/Controllers/BillingApi.java
@@ -0,0 +1,79 @@
+package tdtu.edu.vn.Backend.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import tdtu.edu.vn.Backend.Models.BillingModel;
+import tdtu.edu.vn.Backend.Models.PlanModel;
+import tdtu.edu.vn.Backend.Models.UsersModel;
+import tdtu.edu.vn.Backend.Repositories.BillingRepo;
+import tdtu.edu.vn.Backend.Repositories.PlanRepo;
+import tdtu.edu.vn.Backend.Repositories.UsersRepo;
+import tdtu.edu.vn.Backend.Utilities.Payloads.GeneralResponse;
+import tdtu.edu.vn.Backend.Utilities.Payloads.Home.BillingRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/billing")
+public class BillingApi {
+    @Autowired
+    private BillingRepo billingRepo;
+
+    @Autowired
+    private UsersRepo usersRepo;
+
+    @Autowired
+    private PlanRepo planRepo;
+
+    @GetMapping()
+    public ResponseEntity<?> getByUser(){
+        UsersModel user = usersRepo.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        List<BillingModel> data = billingRepo.findByUsers(user);
+        return ResponseEntity.ok(data);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id){
+        Optional<BillingModel> data = billingRepo.findById(id);
+        if(data.isPresent()){
+            UsersModel user = usersRepo.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+            BillingModel trans = data.get();
+            if(trans.getUsers().getId() == user.getId()){
+                return ResponseEntity.ok(data.get());
+            }else{
+                ResponseEntity.status(HttpServletResponse.SC_FORBIDDEN).body(new GeneralResponse(false, "Not match user get", null));
+            }
+        }
+        return ResponseEntity.status(HttpServletResponse.SC_NOT_FOUND).body(new GeneralResponse(false, "Not found", null));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody BillingRequest request){
+        try{
+            BillingModel data = new BillingModel();
+            UsersModel user = usersRepo.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+            Optional<PlanModel> getPlan = planRepo.findById(request.getPlanId());
+            if(getPlan.isPresent()){
+                PlanModel plan = getPlan.get();
+                data.setAmount(request.getAmount());
+                data.setPayment(request.getPayment());
+                data.setDescription(request.getDescription());
+                data.setPlan(plan);
+                data.setUsers(user);
+                data = billingRepo.save(data);
+                return ResponseEntity.ok(data);
+            }else{
+                return ResponseEntity.status(HttpServletResponse.SC_BAD_REQUEST).body(new GeneralResponse(false, "not found plan", null));
+            }
+        }catch (Exception e){
+            return ResponseEntity.status(HttpServletResponse.SC_BAD_REQUEST).body(new GeneralResponse(false, e.getMessage(), null));
+        }
+    }
+
+}
